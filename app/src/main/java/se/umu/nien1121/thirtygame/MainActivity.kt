be563a0eb8b2f0b451ffad7205d16204ab6b814e
Niklas Enberg <nickeen95@gmail.com>
2022-06-14 09:12:33 +0200

    initial structure

--- app/src/main/java/se/umu/nien1121/thirtygame/MainActivity.kt
+++ app/src/main/java/se/umu/nien1121/thirtygame/MainActivity.kt
@@ -2,10 +2,25 @@ package se.umu.nien1121.thirtygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var throwButton: Button
+    private lateinit var choiceSpinner: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        throwButton = findViewById(R.id.throw_button)
+        choiceSpinner = findViewById(R.id.choice_spinner)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item, resources.getStringArray(R.array.score_choices)
+        )
+        choiceSpinner.adapter = adapter
     }
 }
